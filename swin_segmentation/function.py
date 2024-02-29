d414e5cff7138831ee2853f39b3441b30e4c3148
lim-sorry <prime18k@naver.com>
2024-02-29 12:42:25 +0000

    init swin-segmentation

---
+++ swin_segmentation/function.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.utils.data import DataLoader, Dataset, Sampler
+from torchvision.datasets import OxfordIIITPet
+from torchvision.transforms import *
+
+class Transform(nn.Module):
+    def __init__(self, img_size:int) -> None:
+        super(type(self), self).__init__()
+        self.transform = Compose([
+            PILToTensor(),
+            Resize((img_size, img_size), antialias=True),
+        ])
+        self.augment = Compose([
+            RandomApply([RandomResizedCrop((img_size, img_size), (0.8, 1.0), antialias=True)], 0.5),            
+            RandomHorizontalFlip(0.5),
+            RandomAutocontrast(0.1),
+            RandomGrayscale(0.1),
+        ])
+
+    def forward(self, img, trg) -> list[Tensor, Tensor]:
+        img = self.transform(img)
+        img = self.augment(img)
+        img = (img / 255.0)
+        img = (img - .5) / 0.5
+        trg = self.transform(trg)
+
+        return img, trg
+    
+if __name__ == '__main__':
+    transform = Transform(256)
+    dataset = OxfordIIITPet('~/data', 'trainval', 'segmentation', transform)
+    dataloader = DataLoader(dataset, 64, True, drop_last=True)
+    for img, trg in dataloader:
+        print(img[0][0])
+        break
--- swin_segmentation/main.py
+++ swin_segmentation/main.py
@@ -1,8 +1,57 @@
+import os
+import torch
+from argparse import ArgumentParser, Namespace
+from function import Transform
+from torchvision.datasets import OxfordIIITPet
+from torch.utils.data import DataLoader
 
+def parse() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument('--mode', type=str, default='train')
+
+    parser.add_argument('--img_size', type=int, default=256)
+    parser.add_argument('--data_path', type=str, default='~/data')
+    parser.add_argument('--batch_size', type=int, default=64)
+
+    parser.add_argument('--epoch', type=int, default=1024)
+    parser.add_argument('--pt', type=str, default='segmentation.pt')
+
+    return parser.parse_args()
+
+
+class Trainer:
+    def __init__(self, opt:Namespace) -> None:
+        self.opt = opt
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+        self.transform = Transform(opt.img_size)
+
+
+    def train(self):
+        opt = self.opt
+
+        dataset = OxfordIIITPet(opt.data_path, 'trainval', 'segmentation', self.transform)
+        dataloader = DataLoader(dataset, opt.batch_size, shuffle=True, drop_last=True)
+
+        if os.path.exists(opt.pt):
+            checkpoint = torch.load(opt.pt)
+            ep = checkpoint['ep']
+
+        
+    def test(self):
+        pass
 
 
 def main() -> None:
-    pass
+    opt = parse()
+    
+    trainer = Trainer(opt)
+    if opt.mode.lower() == 'train':
+        trainer.train()
+    if opt.mode.lower() == 'test':
+        trainer.test()
+
 
 
 if __name__ == '__main__':
